ff2ae9786b767e5c59a6eeed5a6ba01d2e1912ad
Илья Нестеров <88728202+ilyhan@users.noreply.github.com>
2024-11-18 00:28:38 +0300

    Вертска главной страницы

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,5 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const today = new Date().toISOString().split('T')[0];
+document.getElementById("flightDate").setAttribute('min', today);
