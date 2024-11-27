7d4f459328fbd1611efdbad7ed2991e253f60194
Илья Нестеров <88728202+ilyhan@users.noreply.github.com>
2024-11-27 23:11:39 +0300

    Просмотр билетов

---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Airline.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class TicketsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TicketsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var tickets = await _context.Tickets
+                .Include(t=>t.Flight)
+                .Include(t=>t.Passenger)
+                .ToListAsync();
+
+            if(tickets == null)
+            {
+                return NotFound();
+            }
+
+            return View(tickets);
+        }
+    }
+}
